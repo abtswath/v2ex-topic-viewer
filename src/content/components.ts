c392089d8743e7c05d65f45ecd9e740dd337e5cc
abtswath <zyapfine@gmail.com>
2023-01-13 14:55:05 +0800

    feat: 增加翻页按钮事件 hook，增加回复事件 hook

--- src/content/components.ts
+++ src/content/components.ts
@@ -1,45 +1,220 @@
-const topicViewStylesheets = `.topic-viewer-container{overflow:auto;border-bottom:1px solid var(--box-border-color)}#Main{margin:0 auto;max-width:770px;width:100%;}`;
-const topicViewerLoadingStyleSheets = `.topic-viewer-loading{height:40px;border-bottom:1px solid var(--box-border-color);line-height:40px;font-weight:bold}.topic-viewer-loading>span{margin:0 5px;opacity:0;animation-delay:0ms;animation-duration:2000ms;animation-timing-function:linear;animation-iteration-count:infinite}@keyframes fade-v{0%{opacity:0}15%{opacity:1}80%{opacity:1}85%{opacity:0}100%{opacity:0}}@keyframes fade-2{15%{opacity:0}30%{opacity:1}80%{opacity:1}85%{opacity:0}100%{opacity:0}}@keyframes fade-e{30%{opacity:0}45%{opacity:1}80%{opacity:1}85%{opacity:0}100%{opacity:0}}@keyframes fade-x{45%{opacity:0}60%{opacity:1}80%{opacity:1}85%{opacity:0}100%{opacity:0}}`;
-const loadingText = ['V', '2', 'E', 'X'];
+const addClass = (el: HTMLElement, classes: string[] | string) => {
+    if (!Array.isArray(classes)) {
+        classes = [classes];
+    }
+    const classNameList = [...el.className.split(' '), ...classes];
+    el.className = classNameList.filter(x => x.trim() !== '').join(' ');
+}
+
+const removeClass = (el: HTMLElement, classes: string[] | string) => {
+    if (!Array.isArray(classes)) {
+        classes = [classes];
+    }
+    el.className = el.className.split(' ').filter(x => classes.indexOf(x) < 0).join(' ');
+}
 
 export class TopicViewer extends HTMLElement {
 
+    private styleSheets = `.topic-viewer-loading{height:40px;line-height:40px;font-weight:bold;position:absolute;top:50%;left:50%;transform:translateX(-50%) translateY(-50%);z-index:99;}.topic-viewer-loading>span{margin:0 5px;opacity:0;animation-delay:0ms;animation-duration:2000ms;animation-timing-function:linear;animation-iteration-count:infinite}@keyframes fade-v{0%{opacity:0}15%{opacity:1}80%{opacity:1}85%{opacity:0}100%{opacity:0}}@keyframes fade-2{15%{opacity:0}30%{opacity:1}80%{opacity:1}85%{opacity:0}100%{opacity:0}}@keyframes fade-e{30%{opacity:0}45%{opacity:1}80%{opacity:1}85%{opacity:0}100%{opacity:0}}@keyframes fade-x{45%{opacity:0}60%{opacity:1}80%{opacity:1}85%{opacity:0}100%{opacity:0}}.topic-viewer-container{height: 100%;width: 100%;overflow:auto;border-bottom:1px solid var(--box-border-color);position: relative;}#Main{margin:0 auto;max-width:770px;width:100%;}.topic-viewer-hide{display:none;}`;
+
+    private loadingText = ['V', '2', 'E', 'X'];
+
     private container: HTMLElement;
+
+    private loadingElement: HTMLElement;
+
+    private currentPage = 1;
+
+    private contentURL: URL | null = null;
+
+    private loading = false;
 
     constructor() {
         super();
-        const style = document.createElement('style');
-        style.textContent = topicViewStylesheets;
         const shadowRoot = this.attachShadow({ mode: 'open' });
-        Array.from(document.querySelectorAll('link[type="text/css"]')).forEach(x => shadowRoot.appendChild(x.cloneNode(true)));
-        document.querySelectorAll('style').forEach(x => shadowRoot.appendChild(x.cloneNode(true)));
-        shadowRoot.appendChild(style);
+        this.appendStyleSheets();
         this.container = document.createElement('div');
         this.container.className = 'topic-viewer-container';
         shadowRoot.appendChild(this.container);
+        this.loadingElement = this.createLoading();
     }
 
     connectedCallback() {
-        const topicHeight = this.getAttribute('topic-height');
-        this.container.style.height = topicHeight ? `calc(100vh - ${topicHeight}px)` : '100vh';
+        const propHeight = this.getAttribute('data-height');
+        if (propHeight !== null) {
+            this.container.style.height = propHeight;
+        }
+        const uri = this.getAttribute('data-uri');
+        if (uri !== null) {
+            this.contentURL = new URL(uri);
+            this.fetchTopic();
+        }
     }
-}
 
-export class TopicViewerLoading extends HTMLElement {
-    constructor() {
-        super();
+    protected removeContent() {
+        this.container.querySelector('#Main')?.remove();
+    }
+
+    protected putContent(content: Node) {
+        this.container.appendChild(content);
+        this.hookPaginationEvent();
+        this.hookReply();
+    }
+
+    protected hookPaginationEvent() {
+        [
+            ...Array.from(this.container.querySelectorAll('a.page_current')),
+            ...Array.from(this.container.querySelectorAll('a.page_normal'))
+        ].forEach(el => {
+            el.addEventListener('click', (e) => this.pageAnchorHandler(e, (el as HTMLAnchorElement).href));
+        });
+        [
+            ...Array.from(this.shadowRoot!.querySelectorAll('td[title=上一页]')),
+            ...Array.from(this.shadowRoot!.querySelectorAll('td[title=下一页]'))
+        ].forEach(el => {
+            const newNode = el.cloneNode(true) as HTMLElement;
+            const newHTML = newNode.outerHTML.replace('onclick', 'disabled_onclick');
+            const tr = document.createElement('tr');
+            tr.innerHTML = newHTML;
+            const newButton = tr.firstChild!.cloneNode(true);
+            (el as HTMLElement).replaceWith(newButton);
+        });
+        this.shadowRoot!.querySelectorAll('td[disabled_onclick]').forEach(el => {
+            const onClickAttrValue = el.getAttribute('disabled_onclick');
+            el.addEventListener('click', (e) => this.pageButtonHandler(e, onClickAttrValue));
+        })
+        this.container.querySelectorAll('input.page_input').forEach(el => {
+            const newNode = el.cloneNode(true) as HTMLElement;
+            const newHTML = newNode.outerHTML.replace('onkeydown', 'disabled_onkeydown');
+            const div = document.createElement('div');
+            div.innerHTML = newHTML;
+            const newInput = div.firstChild!.cloneNode(true);
+            (el as HTMLElement).replaceWith(newInput);
+        });
+        this.container.querySelectorAll('input[disabled_onkeydown]').forEach(el => {
+            el.addEventListener('keydown', (e) => this.pageInputHandler(e as KeyboardEvent, (el as HTMLInputElement)));
+        });
+    }
+
+    protected hookReply() {
+        this.container.querySelector('input[value=回复]')?.parentElement?.parentElement?.addEventListener('submit', (evt: SubmitEvent) => {
+            evt.preventDefault();
+            const form = (evt.target as HTMLFormElement);
+            const action = form.getAttribute('action');
+            const method = form.getAttribute('method');
+            const value = (form.querySelector('#reply_content') as HTMLTextAreaElement).value;
+            const once = (form.querySelector('#once') as HTMLInputElement)?.value;
+            if (action === null || value === null || value.trim() === '') {
+                return;
+            }
+            const formData = new FormData();
+            formData.append('content', value);
+            formData.append('once', once);
+            fetch(action, {
+                method: method || 'post',
+                body: formData
+            })
+                .then(() => {
+                    this.fetchTopic();
+                })
+                .catch(() => { });
+        });
+    }
+
+    private pageInputHandler(e: KeyboardEvent, el: HTMLInputElement) {
+        if (e.code && e.code === 'Enter') {
+            if (el.value.trim() !== '') {
+                const value = Number(el.value);
+                if (!isNaN(value)) {
+                    this.turnThePage(value);
+                }
+            }
+        }
+    }
+
+    private pageButtonHandler(e: Event, str: string | null) {
+        if (str !== null) {
+            const matches = str.match(/p=([\d]+)/);
+            if (matches !== null) {
+                const page = Number(matches[1]);
+                if (!isNaN(page)) {
+                    this.turnThePage(page);
+                }
+            }
+        }
+    }
+
+    private pageAnchorHandler(e: Event, href: string) {
+        e.preventDefault();
+        const url = new URL(href);
+        const p = url.searchParams.get('p');
+        if (p !== null) {
+            const page = Number(p);
+            if (!isNaN(page)) {
+                this.turnThePage(page);
+            }
+        }
+    }
+
+    private turnThePage(page: number) {
+        this.currentPage = page;
+        this.fetchTopic();
+    }
+
+    private getContentURL() {
+        this.contentURL?.searchParams.set('p', `${this.currentPage}`);
+        return this.contentURL?.toString() || '';
+    }
+
+    protected async fetchTopic() {
+        this.removeContent();
+        this.showLoading();
+        const content = await this.fetch();
+        this.hideLoading();
+        content && this.putContent(content);
+    }
+
+    private async fetch() {
+        const response = await fetch(this.getContentURL());
+        const content = await response.text();
+        const parser = new DOMParser();
+        return parser.parseFromString(content, 'text/html').querySelector('#Main')?.cloneNode(true);
+    }
+
+    protected showLoading() {
+        this.loading = true;
+        if (!this.loadingExists()) {
+            this.container.appendChild(this.loadingElement);
+        }
+        removeClass(this.loadingElement, 'topic-viewer-hide');
+    }
+
+    protected hideLoading() {
+        this.loading = false;
+        addClass(this.loadingElement, 'topic-viewer-hide');
+    }
+
+    private loadingExists() {
+        return this.container.querySelector('.topic-viewer-loading') !== null;
+    }
+
+    private appendStyleSheets() {
+        Array.from(document.querySelectorAll('link[type="text/css"]')).forEach(x => this.shadowRoot?.appendChild(x.cloneNode(true)));
+        document.querySelectorAll('style').forEach(x => this.shadowRoot?.appendChild(x.cloneNode(true)));
         const style = document.createElement('style');
-        style.textContent = topicViewerLoadingStyleSheets;
+        style.textContent = this.styleSheets;
+        this.shadowRoot?.appendChild(style);
+    }
+
+    private createLoading() {
         const loading = document.createElement('div');
         loading.className = 'topic-viewer-loading';
-        loadingText.forEach(x => {
+        this.loadingText.forEach(x => {
             const span = document.createElement('span');
             span.style.animationName = `fade-${x.toLowerCase()}`;
             span.innerText = x;
             loading.appendChild(span);
         });
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.appendChild(style);
-        shadowRoot.appendChild(loading);
+        return loading;
     }
 }
--- src/content/content.ts
+++ src/content/content.ts
@@ -1,5 +1,5 @@
 import "@webcomponents/webcomponentsjs";
-import { TopicViewerLoading, TopicViewer } from './components';
+import { TopicViewer } from './components';
 
 export const getParent = (el: HTMLElement, selector: string): HTMLElement | null => {
     if (!el.parentElement) {
@@ -11,142 +11,6 @@ export const getParent = (el: HTMLElement, selector: string): HTMLElement | null
     return getParent(el.parentElement, selector);
 };
 
-interface TopicViewerHandler {
-    open(topicLink: HTMLAnchorElement): void
-    close(): void
-}
-
-abstract class AbstractTopicViewerHandler implements TopicViewerHandler {
-
-    protected loadingElement: HTMLElement | null = null;
-
-    protected topicItem: HTMLElement | null = null;
-
-    public async open(topicLink: HTMLAnchorElement) {
-        this.topicItem = getParent(topicLink, 'div.cell.item');
-        const loading = this.loading();
-        try {
-            const content = await this.fetchTopic(topicLink.href);
-            loading.remove();
-            content && this.put(content);
-        } catch (error) {
-            loading.remove();
-        }
-    }
-
-    protected async fetchTopic(uri: string) {
-        const response = await fetch(uri);
-        const content = await response.text();
-        const parser = new DOMParser();
-        return parser.parseFromString(content, 'text/html').querySelector('#Main')?.cloneNode(true);
-    }
-
-    public abstract close(): void;
-
-    protected abstract loading(): HTMLElement;
-
-    protected abstract put(content: Node): void;
-
-    protected createLoading(): HTMLElement {
-        return document.createElement('topic-viewer-loading');
-    }
-
-    protected createTopicViewer(content: Node) {
-        const topicViewer = document.createElement('topic-viewer');
-        topicViewer.shadowRoot?.querySelector('div.topic-viewer-container')?.appendChild(content);
-        return topicViewer;
-    }
-}
-
-class RightContainerHandler extends AbstractTopicViewerHandler implements TopicViewerHandler {
-
-    private container: HTMLElement | null = null;
-
-    private styleElement: HTMLElement | null = null;
-
-    private stylesheets = `body{width:50%}.right-topic-viewer{position:fixed;right:0;top:0;width:50%;height:100vh;z-index:9999;box-shadow:0 2px 3px rgb(0 0 0 / 10%);border-bottom:1px solid var(--box-border-color);background-color:var(--box-background-color)}`;
-
-    private createStyleSheets() {
-        this.styleElement = document.createElement('style');
-        this.styleElement.textContent = this.stylesheets;
-        document.head.appendChild(this.styleElement!);
-    }
-
-    protected createContainer() {
-        this.createStyleSheets();
-        const container = document.createElement('div');
-        container.className = 'right-topic-viewer';
-        document.body.appendChild(container);
-        return container;
-    }
-
-    protected loading(): HTMLElement {
-        if (!this.container) {
-            this.container = this.createContainer();
-        } else {
-            this.container.childNodes.forEach(x => x.remove());
-        }
-        const loading = this.createLoading();
-        if ((loading.shadowRoot?.querySelector('.topic-viewer-loading') as HTMLElement)) {
-            (loading.shadowRoot?.querySelector('.topic-viewer-loading') as HTMLElement).style.border = 'none';
-            (loading.shadowRoot?.querySelector('.topic-viewer-loading') as HTMLElement).style.position = 'absolute';
-            (loading.shadowRoot?.querySelector('.topic-viewer-loading') as HTMLElement).style.top = '50%';
-            (loading.shadowRoot?.querySelector('.topic-viewer-loading') as HTMLElement).style.left = '50%';
-            (loading.shadowRoot?.querySelector('.topic-viewer-loading') as HTMLElement).style.transform = 'translateX(-50%) translateY(-50)';
-        }
-        this.container.appendChild(loading);
-        return loading;
-    }
-
-    protected put(content: Node): void {
-        this.container?.appendChild(this.createTopicViewer(content));
-    }
-
-    public close(): void {
-        this.container?.remove();
-        this.styleElement?.remove();
-        this.container = null;
-        this.styleElement = null;
-    }
-
-}
-
-class UnderTopicHandler extends AbstractTopicViewerHandler implements TopicViewerHandler {
-
-    private topicViewer: HTMLElement | null = null;
-
-    protected loading(): HTMLElement {
-        this.close();
-        this.topicItem?.scrollIntoView();
-        document.querySelector('topic-viewer')?.remove();
-        const loading = this.createLoading();
-        this.insertAfterTopicItem(loading);
-        return loading;
-    }
-
-    private insertAfterTopicItem(el: HTMLElement) {
-        this.topicItem?.parentElement?.insertBefore(el, this.topicItem.nextElementSibling);
-    }
-
-    protected put(content: Node): void {
-        this.topicViewer = this.createTopicViewer(content);
-        this.topicViewer.setAttribute('topic-height', this.topicItem?.offsetHeight.toString() || '0');
-        this.insertAfterTopicItem(this.topicViewer);
-    }
-
-    public close(): void {
-        this.topicViewer?.remove();
-    }
-
-}
-
-const underTopicHandler = new UnderTopicHandler();
-const rightContainerHandler = new RightContainerHandler();
-
-const isLoading = () => {
-    return document.querySelector('topic-viewer-loading') !== null;
-}
-
 let lastClickedTopic = '';
 
 const isSameAsLastClicked = (href: string) => {
@@ -157,45 +21,78 @@ const isSameAsLastClicked = (href: string) => {
 }
 
 const shouldCloseViewer = (href: string) => {
-    return document.querySelector('topic-viewer') !== null && isSameAsLastClicked(href);
+    return isSameAsLastClicked(href) && document.querySelector('topic-viewer') !== null;
 }
+
+const createRightContainer = () => {
+    const styleSheets = `body{width:50%}.right-topic-viewer{position:fixed;right:0;top:0;width:50%;height:100vh;z-index:9999;box-shadow:0 2px 3px rgb(0 0 0 / 10%);border-bottom:1px solid var(--box-border-color);background-color:var(--box-background-color)}`;
+    const styleElement = document.createElement('style');
+    styleElement.textContent = styleSheets;
+    const container = document.createElement('div');
+    container.className = 'right-topic-viewer';
+    container.appendChild(styleElement);
+    document.body.appendChild(container);
+    return container;
+}
+
+let rightContainer: HTMLElement | null = null;
+
+const insertAfterTopicItem = (topicViewer: HTMLElement, topicItem: HTMLElement) => {
+    rightContainer?.remove();
+    topicViewer.setAttribute('data-height', `calc(100vh - ${topicItem.offsetHeight}px)`);
+    topicItem.scrollIntoView();
+    topicItem.parentElement?.insertBefore(topicViewer, topicItem.nextElementSibling);
+}
+const insertPageRight = (topicViewer: HTMLElement) => {
+    if (rightContainer === null) {
+        rightContainer = createRightContainer();
+    }
+    topicViewer.setAttribute('data-height', '100%');
+    rightContainer.appendChild(topicViewer);
+}
+
+const viewTopic = (evt: MouseEvent) => {
+    if (evt.ctrlKey) {
+        return;
+    }
+    const el = evt.target! as HTMLElement;
+    if (el.tagName.toLowerCase() == 'a' && el.className.indexOf('topic-link') >= 0) {
+        const topicItem = getParent(el, 'div.cell');
+        if (topicItem === null) {
+            return;
+        }
+        evt.preventDefault();
+        const anchor = el as HTMLAnchorElement;
+        if (shouldCloseViewer(anchor.href)) {
+            lastClickedTopic = '';
+            rightContainer?.remove();
+            document.querySelector('topic-viewer')?.remove();
+            return;
+        }
+        document.querySelector('topic-viewer')?.remove();
+        const topicViewer = document.createElement('topic-viewer');
+        topicViewer.setAttribute('data-uri', anchor.href);
+        chrome.storage.local.get('settings')
+            .then((data) => {
+                switch (data?.settings?.containerPosition) {
+                    case 'right':
+                        insertPageRight(topicViewer);
+                        break;
+                    default:
+                        insertAfterTopicItem(topicViewer, topicItem);
+                        break;
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+                insertAfterTopicItem(topicViewer, topicItem);
+            });
+    }
+};
 
 if (document.querySelectorAll('a.topic-link').length > 0) {
-    customElements.define('topic-viewer-loading', TopicViewerLoading);
     customElements.define('topic-viewer', TopicViewer);
-    document.querySelector('#Main>.box')?.addEventListener('click', (evt: Event) => {
-        const el = evt.target! as HTMLElement;
-        if (el.tagName.toLowerCase() == 'a' && el.className.indexOf('topic-link') >= 0) {
-            const topicItem = getParent(el, 'div.cell.item');
-            if (topicItem === null) {
-                return;
-            }
-            evt.preventDefault();
-            if (isLoading()) {
-                return;
-            }
-            const anchor = el as HTMLAnchorElement;
-            if (shouldCloseViewer(anchor.href)) {
-                rightContainerHandler.close();
-                underTopicHandler.close();
-                return;
-            }
-            chrome.storage.local.get('settings')
-                .then((data) => {
-                    switch (data?.settings?.containerPosition) {
-                        case 'right':
-                            rightContainerHandler.open(anchor);
-                            break;
-                        default:
-                            underTopicHandler.open(anchor);
-                            break;
-                    }
-                })
-                .catch((err) => {
-                    console.error(err);
-                    underTopicHandler.open(anchor);
-                });
-        }
+    document.querySelectorAll('a.topic-link').forEach(el => {
+        (el as HTMLElement).addEventListener('click', viewTopic);
     });
 }
-
